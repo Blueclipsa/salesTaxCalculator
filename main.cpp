cbd8ad4b4131a7441947e94817d3e2dce504af45
Portalfan4351 <awesomer32@gmail.com>
2022-07-05 03:14:13 -0500

    this is a test change

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/* version 0.1
+
+This program will take an input of a price and a sales tax rate and return the result
+
+*/
+
+int main()
+{
+    auto price = 0.;
+    auto taxRate = 0.; 
+    auto taxCost = 0.;
+    auto totalPrice = 0.;
+    auto taxInput = 0.;
+    cout << "Input the price of your item before tax: ";
+    cin >> price;
+    cout << endl;
+
+    cout << "Input the sales tax rate: ";
+    cin >> taxInput;
+    cout << endl;
+
+    taxRate = taxInput/100.;
+    
+    taxCost = price * taxRate;
+    totalPrice = price + taxCost;
+
+    cout << "The total cost of your item will be " << totalPrice;
+
+
+}
